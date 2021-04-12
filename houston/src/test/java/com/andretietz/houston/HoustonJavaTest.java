8f75d47f877d818b192f172b18ffffaa3de8a210
André Tietz <andre.tietz@gmail.com>
2021-04-12 16:34:05 +0200

    adding smal test to make sure you can use java as well.

---
+++ houston/src/test/java/com/andretietz/houston/HoustonJavaTest.java
@@ -0,0 +1,21 @@
+package com.andretietz.houston;
+
+import org.junit.Test;
+
+public class HoustonJavaTest {
+
+  /**
+   * This test is only to prove that you can use it in java as well.
+   */
+  @Test
+  public void simpleTest() {
+    Houston.init()
+      .add(message -> {
+      })
+      .launch();
+
+    Houston.send("test")
+      .with("foo", "bar")
+      .over();
+  }
+}
